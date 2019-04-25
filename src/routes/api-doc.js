373cd0b250e5e7a10d808ed84b0298b98aca0a39
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-04-25 12:15:46 +0300

    Add support for Swagger UI

--- src/routes/api-doc.js
+++ src/routes/api-doc.js
@@ -29,13 +29,18 @@
 import fs from 'fs';
 import path from 'path';
 import {Router} from 'express';
+import {SWAGGER_UI_URL} from '../config';
 
 export default function () {
 	const apiDoc = fs.readFileSync(path.join(__dirname, '..', 'api.json'), 'utf8');
 
 	return new Router()
 		.get('/', (req, res) => {
-			res.set('Content-Type', 'application/json');
-			res.send(apiDoc);
+			if (req.accepts('html') && SWAGGER_UI_URL) {
+				res.redirect(SWAGGER_UI_URL);
+			} else {
+				res.set('Content-Type', 'application/json');
+				res.send(apiDoc);
+			}
 		});
 }
