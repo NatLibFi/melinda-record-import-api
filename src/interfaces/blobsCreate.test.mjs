48b2dc65f5694d48829d86d3592b699669851384
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2025-07-10 11:55:11 +0300

    restore create test

---
+++ src/interfaces/blobsCreate.test.mjs
@@ -0,0 +1,91 @@
+import {describe} from 'node:test';
+import assert from 'node:assert';
+import {READERS} from '@natlibfi/fixura';
+
+import mongoFixturesFactory from '@natlibfi/fixura-mongo';
+
+import generateTests from '@natlibfi/fixugen';
+import {validate} from 'uuid';
+import blobsFactory from './blobs.mjs';
+
+describe('interfaces/blobs', () => {
+  let mongoFixtures;
+
+  generateTests({
+    callback,
+    path: [import.meta.dirname, '..', '..', 'test-fixtures', 'blobs', 'create'],
+    recurse: false,
+    useMetadataFile: true,
+    fixura: {
+      failWhenNotFound: true,
+      reader: READERS.JSON
+    },
+    hooks: {
+      before: async () => {
+        await initMongofixtures();
+      },
+      beforeEach: async () => {
+        await mongoFixtures.clear();
+      },
+      afterEach: async () => {
+        await mongoFixtures.clear();
+      },
+      after: async () => {
+        await mongoFixtures.close();
+      }
+    }
+  });
+
+  async function initMongofixtures() {
+    mongoFixtures = await mongoFixturesFactory({
+      rootPath: [import.meta.dirname, '..', '..', 'test-fixtures', 'blobs', 'create'],
+      gridFS: {bucketName: 'blobmetadatas'},
+      useObjectId: true
+    });
+  }
+
+  async function callback({
+    getFixture,
+    loadExpectedDb,
+    expectToFail = false,
+    expectedFailStatus = ''
+  }) {
+    const MONGO_URI = await mongoFixtures.getUri();
+    await mongoFixtures.populate(getFixture('dbContents.json'));
+    const expectedDb = loadExpectedDb ? getFixture('expectedDb.json') : false;
+
+    const inputStream = getFixture({components: ['payload.txt'], reader: READERS.STREAM});
+    const user = getFixture('user.json');
+
+    const blobs = await blobsFactory({MONGO_URI, MELINDA_API_OPTIONS: {}, BLOBS_QUERY_LIMIT: 100, MONGO_DB: ''});
+
+    try {
+      const id = await blobs.create({contentType: 'foo/bar', profile: 'foo', inputStream, date: new Date('2024-07-20'), user});
+      const dump = dumpParser(await mongoFixtures.dump());
+
+      assert.equal(validate(id), true);
+      assert.deepStrictEqual(dump.blobmetadatas, expectedDb.blobmetadatas);
+      assert.equal(expectToFail, false, 'This is expected to succes');
+    } catch (error) {
+      if (!expectToFail) {
+        throw error;
+      }
+      // console.log(error);  // eslint-disable-line
+      assert.equal(expectToFail, true, 'This is expected to fail');
+      assert.equal(error.status, expectedFailStatus);
+    }
+
+    function dumpParser(dump) {
+      // Drop timestamps
+      const blobmetadatas = dump.blobmetadatas.map(blobmetadata => {
+        const {_id, id, modificationTime, creationTime, timestamp, ...rest} = blobmetadata;
+        return {id: 'foo', ...rest};
+      });
+
+      return {
+        blobmetadatas,
+        'blobmetadatas.files': dump['blobmetadatas.files'].map(({filename}) => ({filename}))
+      };
+    }
+  }
+});
